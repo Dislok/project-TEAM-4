399a1ea42c16d3c974f8348735c7de2468c6cf27
ramonezx <ramonezx@outlook.com>
2024-12-19 16:56:05 -0600

    INicio
    Actualización de página de inicio y cuadro de busqueda

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import './inicio.css';
+import Bruts from '../../components/Busqueda/Bruts';
 
 export const Inicio = () => {
   return (
-    <div>Inicio</div>
+    <>
+      
+      <div id="bannerp" >
+      <div className='banner' style={{ backgroundImage: `url("https://masterscoding.com/wp-content/uploads/2020/06/Banner-817x400-05-scaled.jpg")` }}>
+        {
+          <Bruts />
+        }
+      </div>
+      </div>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12'>
+            <div className='team4_logo' >
+              <div className='team4_logo_img' style={{ backgroundImage: `url("https://pluspng.com/img-png/react-logo-png-react-logo-png-img-900-900-free-transparent-react-png-900x900.jpg")` }}>
+
+              </div>
+            </div>
+          </div>
+          <div className='team4_titulos'>
+            <h1>PROYECTO TEAM 4</h1>
+          </div>
+          <div className='team4_content'>
+            <p>Pagina desarrollada como proyecto final del curso React</p>
+              
+          </div>
+        </div>
+       
+
+      </div>
+    </>
   )
 }
