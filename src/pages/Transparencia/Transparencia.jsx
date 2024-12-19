e041b7bbfc5beb6f7d3d266b9e365b47b0ed40f4
Pablo Hernán Muñoz García <phernanmg@gmail.com>
2024-12-19 11:09:38 -0600

    Busqueda en transparencia

--- src/pages/Transparencia/Transparencia.jsx
+++ src/pages/Transparencia/Transparencia.jsx
@@ -1,11 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router";
 import "./Transparencia.css";
 import { useTransparencia } from "../../hooks";
-
+import Busqueda from "../../components/Busqueda/Busqueda";
 
 export const Transparencia = () => {
   const { datos } = useTransparencia();
+
+  const [busqueda, setBusqueda] = useState('');
+  const [resultados, setResultados] = useState([]);
+
+  useEffect(() => {
+    if (datos.length > 0) {
+      setResultados(datos[0]);
+    }
+  }, [datos]);
+
+  const handleBusqueda = (texto) => {
+    setBusqueda(texto);
+    const datosFiltrados = datos[0].filter((item) =>
+      item.nombre.toLowerCase().includes(texto.toLowerCase())
+    );
+    setResultados(datosFiltrados);
+  };
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -13,10 +31,15 @@ export const Transparencia = () => {
   return (
     <div className="container">
       <div className="row">
+        {/* Barra de búsqueda */}
+        <Busqueda busqueda={busqueda} onSearch={handleBusqueda} />
+
         <div className="rubros_titulo">
           <h2>Articulo 69 Ley de Transparencia (48 Rubros)</h2>
         </div>
-        {datos[0].map((item, index) => (
+
+        {/* Mostrar resultados filtrados */}
+        {resultados.map((item, index) => (
           <Link
             key={index}
             to={`/Transparencia/Rubros/${item.nombre}`}
@@ -29,12 +52,15 @@ export const Transparencia = () => {
                     "http://cdn.hidalgo.gob.mx/plantilla_secretarial/Rubros/PNG/" +
                     item.icono
                   }
+                  alt={item.nombre}
                 />
               </div>
               <h3>{item.nombre}</h3>
             </div>
           </Link>
         ))}
+
+        {/* Enlace adicional */}
         <Link className="rubros_historicos" to="RubrosHistoricos">
           Información Ejercicios Anteriores Art. 69 (48 Fracciones)
         </Link>
@@ -42,4 +68,3 @@ export const Transparencia = () => {
     </div>
   );
 };
-
